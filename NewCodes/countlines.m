b4575d92cde66d2dea3e6e87776fac29c739e35d
Juan C. Meza <jcmeza@ucmerced.edu>
2015-08-14 14:56:01 -0700

    added countlines
    Beta version of routine to count number of lines in a csv file

---
+++ NewCodes/countlines.m
@@ -0,0 +1,13 @@
+function [numlines] = countlines(filename)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+% count lines
+
+fid = fopen(filename,'rt');git
+a   = fread(fid,inf,'*char');
+is  = a==char(13);
+numlines = sum(is);
+fclose(fid);
+
+end
+
